baf84b0015682c011b75e331a7e4da40e1068fd6
Ilya Adiyanov <iadiyanov@gmail.com>
2026-03-26 12:51:33 +0500

    Fix variables and add bucket name to CI

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "cloud_id" {
-  description = "b1gebmdjru11541qtun7"
+  description = "ID моего облака в Яндексе"
   type        = string
 }
 
 variable "folder_id" {
-  description = "b1gnmv7jp2s2an2l9j5i"
+  description = "ID моей папки в Яндексе"
   type        = string
 }
 
 variable "site_bucket_name" {
-  description = "orleantum-tf-intro-site-bucket"
+  description = "Имя бакета для сайта"
   type        = string
 }
